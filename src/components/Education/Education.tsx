8caf72fd03b65f8703d9999f4d8cc0cb438964a2
Gabriel Molina <gabrielmolina717@gmail.com>
2025-07-22 12:26:51 +0800

    added Education functionality - user can input and save their education data - education data will be displayed in the website

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,47 +1,62 @@
 import { useState } from "react";
 
 interface EducationObject {
+  id: string;
   schoolName: string;
   degree: string;
   startDate: string;
   endDate: string;
 }
 
-interface InputEducation {
+interface InputEducationProps {
   education: EducationObject;
-  handleSchoolName: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleDegree: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleStartDate: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleEndDate: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChange: (key: keyof EducationObject, value: string) => void;
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 }
 
 function InputEducation({
   education,
-  handleSchoolName,
-  handleDegree,
-  handleStartDate,
-  handleEndDate,
-}: InputEducation) {
+  handleChange,
+  handleSubmit,
+}: InputEducationProps) {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>School Name: </label>
-        <input type="text" />
+        <input
+          type="text"
+          required
+          value={education.schoolName}
+          onChange={(event) => handleChange("schoolName", event.target.value)}
+        />
       </div>
 
       <div>
         <label>Degree: </label>
-        <input type="text" />
+        <input
+          type="text"
+          required
+          value={education.degree}
+          onChange={(event) => handleChange("degree", event.target.value)}
+        />
       </div>
 
       <div>
         <label>Start Date: </label>
-        <input type="text" />
+        <input
+          type="text"
+          value={education.startDate}
+          onChange={(event) => handleChange("startDate", event.target.value)}
+        />
       </div>
 
       <div>
         <label>End Date: </label>
-        <input type="text" />
+        <input
+          type="text"
+          value={education.endDate}
+          onChange={(event) => handleChange("endDate", event.target.value)}
+        />
       </div>
 
       <button type="submit">Save</button>
@@ -49,21 +64,71 @@ function InputEducation({
   );
 }
 
+interface OutputEducationProps {
+  educationList: EducationObject[];
+}
+
+function OutputEducation({ educationList }: OutputEducationProps) {
+  return (
+    <div>
+      {educationList.map((education) => (
+        <div>
+          <p>{education.schoolName}</p>
+          <p>{education.degree}</p>
+          <p>{education.startDate}</p>
+          <p>{education.endDate}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 function Education() {
-  const educationObject: EducationObject = {
+  const [education, setEducation] = useState<EducationObject>({
+    id: "",
     schoolName: "",
     degree: "",
     startDate: "",
     endDate: "",
-  };
+  });
+  const [educationList, setEducationList] = useState<EducationObject[]>([]);
 
-  const [education, setEducation] = useState(educationObject);
-  const [educationList, setEducationList] = useState([]);
+  function handleChange(key: keyof EducationObject, value: string) {
+    setEducation((prevEducation) => ({ ...prevEducation, [key]: value }));
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const newEducation = {
+      ...education,
+      id: crypto.randomUUID(),
+    };
+
+    setEducationList((prevEducationList) => [
+      ...prevEducationList,
+      newEducation,
+    ]);
+
+    setEducation({
+      id: "",
+      schoolName: "",
+      degree: "",
+      startDate: "",
+      endDate: "",
+    });
+  }
 
   return (
     <div>
       <h2>Education</h2>
-      <InputEducation education={education} />
+      <InputEducation
+        education={education}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+
+      <OutputEducation educationList={educationList} />
     </div>
   );
 }
